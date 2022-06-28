f8c2f3f8d32da06b35bfd0f490e10cbdfb092645
Mohit Ganna <mohitganna@gmail.com>
2022-06-27 21:39:29 -0400

    Created apps

--- abatix/settings.py
+++ abatix/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'masters.apps.MastersConfig',
+    'sales.apps.SalesConfig',
+    'stock.apps.StockConfig',
+    'vouchers.apps.VouchersConfig'
 ]
 
 MIDDLEWARE = [
