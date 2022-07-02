e895f98df73cd72b18a7961bb93f254f89372f33
Mohit Ganna <mohitganna@gmail.com>
2022-07-02 19:57:10 -0400

    Created home page

--- masters/urls.py
+++ masters/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import HomeView
 
 urlpatterns = [
-    path("", LoginView.as_view(template_name="authentication/login.html"), name="login")
+    path("", LoginView.as_view(template_name="masters/login.html"), name="login"),
+    path("home", HomeView.as_view(), name='home')
 ]
