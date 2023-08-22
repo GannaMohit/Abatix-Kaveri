d9cf9fe87448908cc9a983d396d726c96b460280
Mohit Ganna <mohitganna@gmail.com>
2023-08-22 14:29:10 +0530

    Registered masters admin models

--- masters/admin.py
+++ masters/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(GST_State)
+admin.site.register(GST_Rate)
+admin.site.register(HSN)
+
+admin.site.register(Metal)
+admin.site.register(Type)
+admin.site.register(Purity)
+admin.site.register(Category)
+admin.site.register(Stud_Type)
+admin.site.register(Unit)
