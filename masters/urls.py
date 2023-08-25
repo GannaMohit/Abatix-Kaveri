ee645d4e7d40c535be89258fdc4f6a29a4ea3428
Mohit Ganna <mohitganna@gmail.com>
2023-08-25 18:49:53 +0530

    Implemented Dashboard (almost)

--- masters/urls.py
+++ masters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import HomeView, UnitsFetchAjax
+from .views import HomeView, UnitsFetchAjax, DashboardView
 
 urlpatterns = [
     path("", LoginView.as_view(template_name="masters/login.html", redirect_authenticated_user=True), name="login"),
     path("home", HomeView.as_view(), name='home'),
+    path("dashboard", DashboardView.as_view(), name='dashboard'),
     path("masters/_fetch_units", UnitsFetchAjax.as_view(), name='fetch_units')
 ]
--- masters/views.py
+++ masters/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Count, Sum
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -7,14 +7,40 @@ from django.http import JsonResponse
 from .models import Unit
 from django.core import serializers
 
+from stock.models import Product
+from vouchers.models import Voucher
+
 import json
+import datetime
 
 # Create your views here.
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "masters/home.html"
 
-class UnitsFetchAjax(View):
+class UnitsFetchAjax(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         units = serializers.serialize("json", Unit.objects.all())
         units_dict = {"units": json.loads(units)}
         return JsonResponse(units_dict)
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "masters/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        today = datetime.date.today()
+        last_april_date = datetime.date(today.year, 4, 1)
+        if last_april_date > today:
+            last_april_date = last_april_date.replace(year=today.year - 1)
+        
+        context["start_date"] = self.request.GET.get("start_date", last_april_date.strftime('%Y-%m-%d'))
+        context["end_date"] = self.request.GET.get("end_date", today.strftime('%Y-%m-%d'))
+
+        context['metal_opening'] = sum(v.pure_weight for v in Voucher.objects.filter(type="Receive", date__lt=context["start_date"]))
+        context['metal_purchase'] = sum(v.pure_weight for v in Voucher.objects.filter(type="Receive", date__gte=context["start_date"], date__lte=context["end_date"]))
+        context['metal_sale'] = sum(v.pure_weight for v in Voucher.objects.filter(type="Issue", date__gte=context["start_date"], date__lte=context["end_date"]))
+        context['metal_inhand'] = context['metal_opening'] + context['metal_purchase'] - context['metal_sale']
+
+        context["stock"] = Product.objects.filter(sold=False).values('metal__metal', 'purity__purity', 'type__type', 'category__category').annotate(Count('pk'), Sum('gross_weight'), Sum('studs_weight'), Sum('net_weight'))
+        return context
