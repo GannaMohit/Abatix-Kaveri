e190624a2a127955b62503b1c31eea11350e9557
Mohit Ganna <mohitganna@gmail.com>
2023-08-29 17:36:01 +0530

    Implemented Logout

--- masters/urls.py
+++ masters/urls.py
@@ -4,6 +4,7 @@ from .views import HomeView, UnitsFetchAjax, DashboardView, TaxesFetchAjax, Cust
 
 urlpatterns = [
     path("", LoginView.as_view(template_name="masters/login.html", redirect_authenticated_user=True), name="login"),
+    path("logout", LogoutView.as_view(), name='logout'),
     path("home", HomeView.as_view(), name='home'),
     path("dashboard", DashboardView.as_view(), name='dashboard'),
     path("masters/_fetch_units", UnitsFetchAjax.as_view(), name='fetch_units'),
