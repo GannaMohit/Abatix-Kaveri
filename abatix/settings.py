c2e2add5c5118388efc1c1233e80b4a8480f2881
Mohit Ganna <mohitganna@gmail.com>
2023-09-01 16:47:35 +0530

    Small path fix

--- abatix/settings.py
+++ abatix/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3(6p!^=rw*ry$=nm$96q!zt&-@ckt2=s4v1_^g_)r)kj_mc8!'
+SECRET_KEY = os.environ.get('SECRET_KEY',"django-insecure-m3(6p!^=rw*ry$=nm$96q!zt&-@ckt2=s4v1_^g_)r)kj_mc8!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
